8c4f7334eef81bb2a8341e263704504232c34d4d
Satyam998413 <84256320+Satyam998413@users.noreply.github.com>
2022-06-08 17:18:30 +0530

    Today I am making models and routes

--- BackEnd/routes/auth.js
+++ BackEnd/routes/auth.js
@@ -2,13 +2,16 @@ const express = require('express');
 const router=express.Router();
 
 
-
+// Create a user using post ./api/auth Does not required auth
 router.get("/",(req,res)=>{
-    obj={
-        number:1,
-        name:"Satyam"
-    }
-    res.json(obj);
+    // obj={
+    //     number:1,
+    //     name:"Satyam"
+    // }
+    // res.json(obj);
+    console.log(req.body);
+    res.send("Hello Afd");
+
 })
 
 module.exports=router;
