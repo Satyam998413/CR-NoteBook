5dc407ed29f1a859e00528657cba86dc6beb5081
Satyam998413 <84256320+Satyam998413@users.noreply.github.com>
2022-06-16 23:12:14 +0530

    Today added crud operation on my notes

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import {
   Route
 } from "react-router-dom";
 import Navbar from './components/Navbar';
-import { Home } from './components/Home';
-import About from './components/About';
+// import { Home } from './components/Home';
+// import About from './components/About';
 import NoteState from './context/notes/NoteState';
 import { Alert } from './components/Alert';
-import Signup from './components/Signup';
-import Login from './components/Login';
+// import Signup from './components/Signup';
+// import Login from './components/Login';
 
 
 function App() {
@@ -22,7 +22,7 @@ function App() {
           <Navbar />
           <Alert message="This is amazing React course" />
           <div className="container">
-            <Switch>
+            {/* <Switch>
               <Route exact path="/">
                 <Home />
               </Route>
@@ -35,7 +35,7 @@ function App() {
               <Route exact path="/signup">
                 <Signup />
               </Route>
-            </Switch>
+            </Switch> */}
           </div>
         </Router>
       </NoteState>
