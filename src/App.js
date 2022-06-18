424ac2ba4e852f3d13b76e7b79240d6f308c1a73
Satyam998413 <84256320+Satyam998413@users.noreply.github.com>
2022-06-18 14:07:03 +0530

    Today added my navbar component and router setup

--- src/App.js
+++ src/App.js
@@ -1,45 +1,39 @@
 
 import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
+import About from './components/About';
 import Navbar from './components/Navbar';
-// import { Home } from './components/Home';
-// import About from './components/About';
-import NoteState from './context/notes/NoteState';
-import { Alert } from './components/Alert';
-// import Signup from './components/Signup';
-// import Login from './components/Login';
+import Alert from './components/Alert';
+import Signup from './components/Signup';
+import Contact from './components/Contact';
+
+
+
+
+
+
 
 
 function App() {
   return (
-    <>
-      <NoteState>
-        <Router>
-          <Navbar />
-          <Alert message="This is amazing React course" />
-          <div className="container">
-            {/* <Switch>
-              <Route exact path="/">
-                <Home />
-              </Route>
-              <Route exact path="/about">
-                <About />
-              </Route>
-              <Route exact path="/login">
-                <Login />
-              </Route>
-              <Route exact path="/signup">
-                <Signup />
-              </Route>
-            </Switch> */}
-          </div>
-        </Router>
-      </NoteState>
-    </>
+    <BrowserRouter>
+    
+    <Navbar/>
+    <Alert message="this is wrong way!" />
+ <Routes>
+    
+          <Route exact path="/" element={<About />} />
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/contact" element={<Contact />} />
+          <Route exact  path="/signup" element={<Signup />} />
+        
+      </Routes>
+ 
+  
+    </BrowserRouter>
   );
 }
 
--- src/components/Alert.js
+++ src/components/Alert.js
@@ -9,3 +9,5 @@ export const Alert = (props) => {
         </div>
     )
 }
+
+export default Alert;
